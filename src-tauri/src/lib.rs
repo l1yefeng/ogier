005331517cdab37b94b1de33c7461dde5b26bb15
LYF <iamlazynic@gmail.com>
2025-03-29 18:23:38 +0000

    ArrowRight: see the next chapter

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,25 +1,68 @@
-use epub::doc::{DocError, EpubDoc};
+use epub::doc::EpubDoc;
+use std::fs::File;
+use std::io::BufReader;
+use std::sync::Mutex;
 use tauri_plugin_dialog::DialogExt;
 
-fn get_book_title(path: &str) -> Result<String, DocError> {
-    match EpubDoc::new(path) {
-        Ok(book) => Ok(book.mdata("title").unwrap_or("(no title)".to_owned())),
-        Err(e) => Err(e),
+struct AppState {
+    book: Mutex<Option<EpubDoc<BufReader<File>>>>,
+}
+
+impl AppState {
+    fn new() -> Self {
+        Self {
+            book: Mutex::new(None),
+        }
     }
 }
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn open_epub(app_handle: tauri::AppHandle) -> String {
-    let file_path = app_handle
+fn open_epub(app: tauri::AppHandle, state: tauri::State<AppState>) -> String {
+    let Some(file_path) = app
         .dialog()
         .file()
         .add_filter("EPUB", &["epub"])
-        .blocking_pick_file();
-    match file_path {
-        Some(path) => get_book_title(&path.to_string()).unwrap_or("(open failed)".to_owned()),
-        None => String::new(),
+        .blocking_pick_file()
+    else {
+        return String::new(); // did not choose
+    };
+    // open file
+    let Ok(mut book) = EpubDoc::new(&file_path.to_string()) else {
+        eprintln!("ERR: Failed to open file.");
+        return String::new();
+    };
+    // set state and get content
+    let Ok(mut book_guard) = state.book.lock() else {
+        eprintln!("ERR: Failed to acquire lock.");
+        return String::new();
+    };
+    let current_chapter_content = book.get_current_with_epub_uris();
+    *book_guard = Some(book);
+    drop(book_guard);
+    let Ok(content) = current_chapter_content else {
+        eprintln!("ERR: Failed to extract content.");
+        return String::new();
+    };
+    String::from_utf8(content).unwrap_or_default()
+}
+
+#[tauri::command]
+fn next_chapter(_app: tauri::AppHandle, state: tauri::State<AppState>) -> String {
+    let Ok(mut book_guard) = state.book.lock() else {
+        return String::new();
+    };
+    let book = book_guard.as_mut().unwrap();
+    if !book.go_next() {
+        drop(book_guard);
+        return String::new();
     }
+    let current_chapter_content = book.get_current_with_epub_uris();
+    drop(book_guard);
+    let Ok(content) = current_chapter_content else {
+        eprintln!("ERR: Failed to extract content.");
+        return String::new();
+    };
+    String::from_utf8(content).unwrap_or_default()
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -27,7 +70,8 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![open_epub])
+        .manage(AppState::new())
+        .invoke_handler(tauri::generate_handler![open_epub, next_chapter])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,36 @@
 import { invoke } from "@tauri-apps/api/core";
 
-document.body.onclick = async () => {
-	const result: string = await invoke("open_epub");
-	alert(`Opened "${result}"!`);
-};
+async function openEpub() {
+    const result: string = await invoke("open_epub");
+    if (!result) {
+        window.alert("Could not open.");
+        return;
+    }
+
+    const parser = new DOMParser();
+    const epubPageDoc = parser.parseFromString(result, "application/xhtml+xml");
+    document.body.replaceChildren(...epubPageDoc.body.children);
+}
+
+async function goToNextChapter() {
+    const result: string = await invoke("next_chapter");
+    if (!result) {
+        return;
+    }
+
+    const parser = new DOMParser();
+    const epubPageDoc = parser.parseFromString(result, "application/xhtml+xml");
+    document.body.replaceChildren(...epubPageDoc.body.children);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	const elemClickToOpen = document.getElementById("click-to-open");
+	elemClickToOpen?.addEventListener("click", openEpub);
+
+    document.body.addEventListener("keyup", (event) => {
+        if (event.key === "ArrowRight") {
+            event.preventDefault();
+            goToNextChapter();
+        }
+    });
+});
