79caa4f259ec4db4e6d6f2c7cc2c8a6855ce8d51
LYF <iamlazynic@gmail.com>
2025-03-30 17:51:44 +0100

    Can go prev and go next; can see images

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,4 @@
+use base64::{Engine as _, engine::general_purpose};
 use epub::doc::EpubDoc;
 use std::fs::File;
 use std::io::BufReader;
@@ -17,7 +18,40 @@ impl AppState {
 }
 
 #[tauri::command]
-fn open_epub(app: tauri::AppHandle, state: tauri::State<AppState>) -> String {
+fn fetch_resource(_app: tauri::AppHandle, state: tauri::State<AppState>, path: String) -> String {
+    let Ok(mut book_guard) = state.book.lock() else {
+        return String::new();
+    };
+    let book = book_guard.as_mut().unwrap();
+    let Some(content) = book.get_resource_by_path(&path) else {
+        eprintln!("ERR: get_resource_by_path({}) returned None.", &path);
+        return String::new();
+    };
+    let Some(mime) = book.get_resource_mime_by_path(&path) else {
+        eprintln!("ERR: get_resource_mime_by_path returned None.");
+        return String::new();
+    };
+    drop(book_guard);
+
+    if mime.starts_with("image/") {
+        let mut buf = mime;
+        buf.push_str(";base64,");
+        general_purpose::STANDARD.encode_string(content, &mut buf);
+        buf
+    } else if mime.starts_with("text/") {
+        String::from_utf8(content).unwrap_or_default()
+    } else {
+        eprintln!("ERR: Unexpected mime type: {}", mime);
+        String::new()
+    }
+}
+
+#[tauri::command]
+fn open_epub(
+    app: tauri::AppHandle,
+    state: tauri::State<AppState>,
+    window: tauri::Window,
+) -> String {
     let Some(file_path) = app
         .dialog()
         .file()
@@ -31,6 +65,22 @@ fn open_epub(app: tauri::AppHandle, state: tauri::State<AppState>) -> String {
         eprintln!("ERR: Failed to open file.");
         return String::new();
     };
+    for (id, (path, mime)) in book.resources.iter() {
+        println!(
+            "Resource #{}\t{} ({})",
+            id,
+            path.to_str().unwrap_or("(non utf8 path)"),
+            mime
+        );
+    }
+    book.mdata("title").and_then(|title| {
+        let Ok(_) = window.set_title(&title.to_string()) else {
+            eprintln!("ERR: Failed to set window title.");
+            return None;
+        };
+        Some(())
+    });
+
     // set state and get content
     let Ok(mut book_guard) = state.book.lock() else {
         eprintln!("ERR: Failed to acquire lock.");
@@ -46,13 +96,19 @@ fn open_epub(app: tauri::AppHandle, state: tauri::State<AppState>) -> String {
     String::from_utf8(content).unwrap_or_default()
 }
 
-#[tauri::command]
-fn next_chapter(_app: tauri::AppHandle, state: tauri::State<AppState>) -> String {
+fn goto_chapter(state: tauri::State<AppState>, offset: i32) -> String {
     let Ok(mut book_guard) = state.book.lock() else {
         return String::new();
     };
     let book = book_guard.as_mut().unwrap();
-    if !book.go_next() {
+    let ok = if offset == 1 {
+        book.go_next()
+    } else if offset == -1 {
+        book.go_prev()
+    } else {
+        false
+    };
+    if !ok {
         drop(book_guard);
         return String::new();
     }
@@ -65,13 +121,28 @@ fn next_chapter(_app: tauri::AppHandle, state: tauri::State<AppState>) -> String
     String::from_utf8(content).unwrap_or_default()
 }
 
+#[tauri::command]
+fn next_chapter(_app: tauri::AppHandle, state: tauri::State<AppState>) -> String {
+    goto_chapter(state, 1)
+}
+
+#[tauri::command]
+fn prev_chapter(_app: tauri::AppHandle, state: tauri::State<AppState>) -> String {
+    goto_chapter(state, -1)
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .manage(AppState::new())
-        .invoke_handler(tauri::generate_handler![open_epub, next_chapter])
+        .invoke_handler(tauri::generate_handler![
+            fetch_resource,
+            next_chapter,
+            open_epub,
+            prev_chapter,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,115 @@
 import { invoke } from "@tauri-apps/api/core";
 
-async function openEpub() {
-    const result: string = await invoke("open_epub");
-    if (!result) {
-        window.alert("Could not open.");
-        return;
-    }
+let elemClickToOpen: HTMLElement | null;
+let elemReaderHost: HTMLElement | null;
 
-    const parser = new DOMParser();
-    const epubPageDoc = parser.parseFromString(result, "application/xhtml+xml");
-    document.body.replaceChildren(...epubPageDoc.body.children);
+async function renderBookPage(content: string) {
+	const shadowRoot = elemReaderHost?.shadowRoot;
+	if (!shadowRoot) return;
+
+	const parser = new DOMParser();
+	const epubPageDoc = parser.parseFromString(content, "application/xhtml+xml");
+	for (const elem of epubPageDoc.body.querySelectorAll<HTMLImageElement>(
+		'img[src^="epub://"]',
+	)) {
+		elem.style.visibility = "hidden";
+		const path = elem.src.substring(7);
+		invoke("fetch_resource", { path }).then(base64 => {
+			elem.src = `data:${base64 as string}`;
+			elem.style.visibility = "visible";
+		});
+	}
+	for (const elem of epubPageDoc.body.querySelectorAll<SVGImageElement>("image")) {
+		const href = elem.href.baseVal;
+		if (href.startsWith("epub://")) {
+			elem.style.visibility = "hidden";
+			const path = href.substring(7);
+			invoke("fetch_resource", { path }).then(base64 => {
+				elem.href.baseVal = `data:${base64 as string}`;
+				elem.style.visibility = "visible";
+			});
+		}
+	}
+
+	// TODO (optimize) don't need to re-fetch the same <link>
+	const stylesheets: CSSStyleSheet[] = [];
+	// TODO (optimize) parallellize
+	for (const elemLink of epubPageDoc.head.querySelectorAll<HTMLLinkElement>(
+		'link[rel="stylesheet"]',
+	)) {
+		const path = elemLink.href.substring(7);
+		const css: string = await invoke("fetch_resource", { path });
+		const stylesheet = new CSSStyleSheet();
+		stylesheet.replace(css);
+		stylesheets.push(stylesheet);
+	}
+	let styleElemCss = `
+    img {
+        max-width: 100%;
+    }
+    `;
+	for (const elemStyle of epubPageDoc.head.querySelectorAll<HTMLStyleElement>("style")) {
+		const css = elemStyle.textContent;
+		if (css) {
+			styleElemCss += css;
+		}
+	}
+	const stylesheet = new CSSStyleSheet();
+	stylesheet.replace(styleElemCss);
+	stylesheets.push(stylesheet);
+
+	shadowRoot.adoptedStyleSheets = stylesheets;
+	shadowRoot.replaceChildren();
+	shadowRoot.appendChild(epubPageDoc.body);
+}
+
+async function openEpub() {
+	const result: string = await invoke("open_epub");
+	if (!result) {
+		window.alert("Could not open.");
+		return;
+	}
+
+	// setup reader
+	elemClickToOpen!.remove();
+	elemReaderHost!.attachShadow({ mode: "open" });
+
+	document.body.addEventListener("keyup", handleKeyEvent);
+
+	renderBookPage(result);
 }
 
 async function goToNextChapter() {
-    const result: string = await invoke("next_chapter");
-    if (!result) {
-        return;
-    }
+	const result: string = await invoke("next_chapter");
+	if (!result) {
+		return; // maybe this is the last chapter
+	}
 
-    const parser = new DOMParser();
-    const epubPageDoc = parser.parseFromString(result, "application/xhtml+xml");
-    document.body.replaceChildren(...epubPageDoc.body.children);
+	renderBookPage(result);
+}
+
+async function goToPrevChapter() {
+	const result: string = await invoke("prev_chapter");
+	if (!result) {
+		return; // maybe this is the first chapter
+	}
+
+	renderBookPage(result);
+}
+
+function handleKeyEvent(event: KeyboardEvent) {
+	if (event.key === "ArrowRight") {
+		event.preventDefault();
+		goToNextChapter();
+	} else if (event.key === "ArrowLeft") {
+		event.preventDefault();
+		goToPrevChapter();
+	}
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-	const elemClickToOpen = document.getElementById("click-to-open");
-	elemClickToOpen?.addEventListener("click", openEpub);
+	elemClickToOpen = document.getElementById("click-to-open");
+	elemClickToOpen!.addEventListener("click", openEpub);
 
-    document.body.addEventListener("keyup", (event) => {
-        if (event.key === "ArrowRight") {
-            event.preventDefault();
-            goToNextChapter();
-        }
-    });
+	elemReaderHost = document.getElementById("reader-host");
 });
