7654514358fd3b76c1aa8f98a1b247d84e80b918
LYF <iamlazynic@gmail.com>
2025-04-23 15:32:02 +0100

    Update deps and add script to copy icons

---
+++ tools/copy_fluent_icons.ts
@@ -0,0 +1,28 @@
+const ICONS = [
+  "book",
+  "document_margins",
+  "notebook_arrow_curve_down",
+  "text_font_size",
+  "text_line_spacing",
+];
+
+async function copy(icon: string): Promise<void> {
+  for (let size = 24; size > 0; size -= 2) {
+    const filename = `${icon}_${size}_regular.svg`;
+    const input = Bun.file(
+      `node_modules/@fluentui/svg-icons/icons/${filename}`
+    );
+    if (!(await input.exists())) {
+      continue;
+    }
+    const output = Bun.file(`src/assets/fluent/${filename}`);
+    await Bun.write(output, input);
+    return;
+  }
+
+  throw new Error(`Icon ${icon} not found.`);
+}
+
+await Promise.all(ICONS.map(copy));
+
+export {};
